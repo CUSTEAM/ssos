7fab5cb86f746e166bc749c3ce6eff3b55faec20
John <John@192.168.23.23>
2017-01-06 13:07:02 +0800

    登入驗證改用criteria查詢

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -1,14 +1,14 @@
 package action;
 
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.util.Enumeration;
-import java.util.Map;
+import java.util.List;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 
 import model.Message;
+import model.Wwpass;
 
 /**
  * 登入動作
@@ -17,7 +17,6 @@ import model.Message;
 public class LoginAction extends BaseAction{
 	
 	public String login() throws Exception{
-		
 		Message msg=new Message();
 		if(username.trim().length()<3||password.length()<3){
 			//資料不全			
@@ -38,24 +37,29 @@ public class LoginAction extends BaseAction{
 		        cookies[i].setValue(null);
 		        response.addCookie(cookies[i]);
 			}          
-        }
+        }		
+    	DetachedCriteria c = DetachedCriteria.forClass(Wwpass.class);
+		c.add(Restrictions.eq("username", username));
+		c.add(Restrictions.eq("password", password));
+		List<Wwpass>list=df.getHibernateDAO().getHibernateTemplate().findByCriteria(c);
+		if(list.size()<1){
+			c = DetachedCriteria.forClass(Wwpass.class);
+			c.add(Restrictions.eq("freename", username));
+			c.add(Restrictions.eq("password", password));
+			list=df.getHibernateDAO().getHibernateTemplate().findByCriteria(c);
+		}		
+    	for(int i=0; i<list.size(); i++){
+    		System.out.println(list.get(i).getFreename());
+    	}
 		
-		
-    	//驗證使用者
-    	Map<String,String>user=dm.sqlGetMap("SELECT username, priority FROM wwpass WHERE username='"+username+"' AND password='"+password+"'");
-		
-		if(user==null){
-			user=dm.sqlGetMap("SELECT username, priority FROM wwpass WHERE freename='"+username+"' AND password='"+password+"'");
-		}
-		
-		if(user==null){			
+		if(list.size()<1){			
 			msg.setError("驗證失敗");
 			savMessage(msg);
 			return "unsuccess";
 		}
-		
+		Wwpass user=(Wwpass)list.get(0);
 		//將自訂帳號對應永久帳號
-		username=user.get("username");
+		username=user.getUsername();
 		//寫入登入記錄		
 		df.exSql("INSERT SYS_LOG_ACCESS(userid,action,accip)VALUES('"+username+"','"+request.getServletPath()+"','"+ip+"')");
 		//寫身份cookie
@@ -71,31 +75,31 @@ public class LoginAction extends BaseAction{
     	"' WHERE username='"+username+"'");    	
     	
     	//學生 (目前為學號驗證)
-    	if(user.get("priority").equals("C")){
+    	if(user.getPriority().equals("C")){
     		response.sendRedirect("/stis/MyCalendar");//轉送至stis
     		return null;
     	}
     	
     	//教職員
-    	if(user.get("priority").equals("A")){
+    	if(user.getPriority().equals("A")){
     		StringBuilder sb=new StringBuilder("A,");//職員工基本
     		
-    		if(dm.sqlGetInt("SELECT COUNT(*)FROM Dtime WHERE techid='"+user.get("username")+"'")>0||
-    		dm.sqlGetInt("SELECT COUNT(*)FROM Sdtime WHERE techid='"+user.get("username")+"'")>0||
-    		dm.sqlGetInt("SELECT COUNT(*)FROM Class WHERE tutor='"+user.get("username")+"'")>0){
+    		if(dm.sqlGetInt("SELECT COUNT(*)FROM Dtime WHERE techid='"+username+"'")>0||
+    		dm.sqlGetInt("SELECT COUNT(*)FROM Sdtime WHERE techid='"+username+"'")>0||
+    		dm.sqlGetInt("SELECT COUNT(*)FROM Class WHERE tutor='"+username+"'")>0){
     			sb.append("T,");//教師基本
     		}
     		
-    		if(dm.sqlGetInt("SELECT COUNT(*)FROM CODE_DEPT WHERE assistant='"+user.get("username")+"'")>0){
+    		if(dm.sqlGetInt("SELECT COUNT(*)FROM CODE_DEPT WHERE assistant='"+username+"'")>0){
     			sb.append("S,");//系助理
     		}
     		
-    		if(dm.sqlGetInt("SELECT COUNT(*) FROM empl WHERE idno='"+user.get("username")+"' AND (sname LIKE '%主任%' OR sname LIKE '%長%')")>0){
+    		if(dm.sqlGetInt("SELECT COUNT(*) FROM empl WHERE idno='"+username+"' AND (sname LIKE '%主任%' OR sname LIKE '%長%')")>0){
     			sb.append("C,");
     		}//2階主管
     		
     		
-    		sb.append(dm.sqlGetStr("SELECT unit_module FROM empl WHERE idno='"+user.get("username")+"'"));
+    		sb.append(dm.sqlGetStr("SELECT unit_module FROM empl WHERE idno='"+username+"'"));
     		
     		//寫單位cookie
     		cookie = new Cookie("unit", sb.toString());
